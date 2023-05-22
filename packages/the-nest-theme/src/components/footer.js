11673bd16e6d0b362f89d11a73f64af738675f7d
Walter Clayton <walter.h.clayton@gmail.com>
2023-05-22 20:18:28 +0200

    finished footer basics

--- packages/the-nest-theme/src/components/footer.js
+++ packages/the-nest-theme/src/components/footer.js
@@ -1,6 +1,7 @@
 import { connect, styled, css } from "frontity";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot, faEnvelope, faPhone} from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = ({ state, libraries }) => {
     const data = state.source.get("/contact");
@@ -8,6 +9,7 @@ const Footer = ({ state, libraries }) => {
     const Html2React = libraries.html2react.Component;
 
     return (
+        <>
         <MainContainer className="brown-background">
             <Title className="white-title">CONTACT</Title>
             <ParentContainer>
@@ -39,12 +41,84 @@ const Footer = ({ state, libraries }) => {
                 </OrderTwo>
             </ParentContainer>
         </MainContainer>
+        <FooterContainer>
+            <BottomFooter>
+            <Row>
+                <FooterBlock>
+                    <TitleFooter>Opening Hours</TitleFooter>
+                    <Paragraph className="footer-paragraph">Mon-Fri: 8.15 AM - 5:00 PM</Paragraph>
+                    <Paragraph className="footer-paragraph">Sat: 9:00 AM - 5:00 PM</Paragraph>
+                </FooterBlock>
+                <FooterBlock>
+                    <TitleFooter>
+                        Useful Links
+                    </TitleFooter>
+                    <Paragraph className="footer-paragraph">
+                    <a target="_blank" href="#privacy" css={css`text-decoration: none; color: #6f4e37; border: none; background: none;}`}>
+                        Privacy Policy
+                    </a>
+                    </Paragraph>
+                    <Paragraph className="footer-paragraph">
+                    <a target="_blank" href="#menu" css={css`text-decoration: none; color: #6f4e37; border: none; background: none;}`}>
+                        Menu
+                    </a> 
+                    </Paragraph>
+                </FooterBlock>
+                <FooterBlock>
+                    <TitleFooter>Social Media</TitleFooter>
+                    <Paragraph className="footer-paragraph">
+                    <a target="_blank" href="https://www.facebook.com/people/The-Nest-Coffee-Shop/100063685203702/" css={css`text-decoration: none; border: none; background: none;}`}>
+                        <FontAwesomeIcon icon={faFacebook} size="2x" css={css`padding: 10px; color:#6f4e37;:hover{color: #F18888;}`}/>
+                    </a>
+                    <a target="_blank" href="https://www.instagram.com/thenest_bramley/" css={css`text-decoration: none; border: none; background: none;}`}>
+                        <FontAwesomeIcon icon={faInstagram} size="2x" css={css`padding: 10px; color: #6f4e37;:hover{color: #F18888;}`}/>
+                    </a>
+                    </Paragraph>
+                </FooterBlock>
+            </Row>
+            <Col className="bottom-footer">
+                <BrownLine></BrownLine>
+                <Paragraph css={css`color:#6f4e37;`}>© 2023 The Nest, All Rights Reserved</Paragraph>
+            </Col>
+            </BottomFooter>
+        </FooterContainer>
+        </>
     );
 };
 
 const breakpoints = [576, 768, 992, 1200];
 
 const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+
+const BrownLine = styled.hr`
+border: 1px solid #A67B5B;
+border-radius: 1px;
+width: 300px;
+margin: 10px;
+`;
+
+const Col = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 50px;
+${mq[2]} {
+    margin-top: 0px;
+}
+`;
+
+const Row = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+${mq[2]} {
+    flex-direction: column;
+    text-align: center;
+    width: 100%;
+    margin: -10px 0 20px 0;
+    border-radius: 0; 
+}
+`;
 
 const MainContainer = styled.div`
 display: flex;
@@ -72,6 +146,21 @@ ${mq[2]} {
     border-radius: 0; 
 }
 `;
+
+const BottomFooter = styled.div`
+
+`;
+
+const FooterContainer = styled.div`
+`;
+
+const FooterBlock = styled.div`
+text-align: center;
+width: 33%;
+${mq[2]} { 
+    width: 100%;
+    margin-bottom: 25px;
+}`;
 
 const OrderTwo = styled.div`
 align-items: center;
@@ -101,11 +190,26 @@ ${mq[2]} {
 }
 `;
 
-const Title = styled.h2`font-size: 38px; font-weight: 700; color: #6f4e37; margin-bottom: 2rem; ${mq[2]} { font-size: 2rem; margin-bottom: 2rem; } &.white-title { color: #ffffff; }`;
+const Title = styled.h2`
+font-size: 32px;
+font-weight: 600;
+color: #6f4e37;
+margin-bottom: 10px;
+${mq[2]} { font-size: 22px; margin-bottom: 10px; } &.white-title { color: #ffffff; }`;
+
+const TitleFooter = styled.h3`
+font-size: 28px;
+font-weight: 600;
+color: #6f4e37;
+margin-bottom: 10px;
+${mq[2]} { font-size: 28px; margin-bottom: 10px; } &.white-title { color: #ffffff; }`;
+
+
 
 const Paragraph = styled.p`
 font-size: 22px;
 padding: 10px 10px;
+&.footer-paragraph{color: #6f4e37;}
 `;
 
 const ContactForm = styled.div`
@@ -138,13 +242,19 @@ const ContactForm = styled.div`
     border-radius: 5px;
     padding: 10px;
     width: fit-content;
-  }
-  div > div > div:last-child{
+    ${mq[2]} {
+        margin: 10px auto 30px auto;
+        }
+    }
+    div > div > div:last-child{
     background-color: #F18888;
     border-radius: 5px;
     padding: 10px;
     width: fit-content;
     border: 1px solid transparent;
+    ${mq[2]} {
+        margin: 10px auto 30px auto;
+    }
   }
 `;
 
